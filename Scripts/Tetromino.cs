a6ddb212c64e24cdc863679f91735708f720b35a
Rulystik <rulya_@ukr.net>
2021-03-27 17:30:36 +0200

    + cs

---
+++ Scripts/Tetromino.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour
+{
+    Game game;
+    private void Update() 
+    {
+        
+    }
+    private void Start() 
+    {
+        game = GetComponent<Game>();
+    }
+    
+}
